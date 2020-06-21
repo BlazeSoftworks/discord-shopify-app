1ccbbec47901a9010b712c0029869aa37f42767b
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-06-21 10:41:06 +0300

    third attempt

--- server.js
+++ server.js
@@ -97,9 +97,11 @@ app.prepare().then(() => {
     return
   });
 
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  // server.listen(port, () => {
+  //   console.log(`> Ready on http://localhost:${port}`);
+  // });
+
+  app.listen(process.env.PORT);
 });
 
 
