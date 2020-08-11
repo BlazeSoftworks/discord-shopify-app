9553e537f1f75cf4b9313b167199fc0d098842ab
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-12 01:13:56 +0300

    fasfas

--- src/server.js
+++ src/server.js
@@ -75,6 +75,7 @@ app.prepare().then(() => {
         'write_products',
         'read_script_tags',
         'write_script_tags',
+        'read_reports'
       ],
       async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
