51fa13e310088509d490b7b08a6ebc3a856499c4
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-12 20:36:47 +0300

    PLM

--- src/requests/getSubQuery.js
+++ src/requests/getSubQuery.js
@@ -1,48 +1,58 @@
 const getSubscriptionQuery = async (ctx, accessToken, shop) => {
 
-    const query = JSON.stringify({
-        query: `query {
-            currentAppInstallation {
-              rawSubscriptions(first: 1) {
-                edges {
-                  node {
-                    lineItems {
-                      plan {
-                        pricingDetails {
-                          __typename
-                          ... on AppRecurringPricing {
-                            price {
-                              amount
-                              currencyCode
-                            }
-                          }                          
+  const query = JSON.stringify({
+    query: `query {
+          currentAppInstallation {
+            allSubscriptions(first: 2) {
+              edges {
+                node {
+                  lineItems {
+                    plan {
+                      pricingDetails {
+                        __typename
+                        ... on AppRecurringPricing {
+                          price {
+                            amount
+                            currencyCode
+                          }
+                        }
+                        ... on AppUsagePricing {
+                          balanceUsed {
+                            amount
+                            currencyCode
+                          }
+                          cappedAmount {
+                            amount
+                            currencyCode
+                          }
                         }
                       }
                     }
-                    createdAt
-                    id
-                    name
-                    status
-                    test
                   }
+                  createdAt
+                  id
+                  name
+                  status
+                  test
                 }
               }
             }
-          }`
-    });
+          }
+        }`
+  });
 
-    const response = await fetch(`https://${shop}/admin/api/2020-07/graphql.json`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            "X-Shopify-Access-Token": accessToken,
-        },
-        body: query
-    })
+  const response = await fetch(`https://${shop}/admin/api/2020-07/graphql.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "X-Shopify-Access-Token": accessToken,
+    },
+    body: query
+  })
 
-    const responseJson = await response.json();
+  const responseJson = await response.json();
 
-    return responseJson
+  return responseJson
 }
 
 module.exports = getSubscriptionQuery;
