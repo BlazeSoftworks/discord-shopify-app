248989367c1618ae99344226ae0dc4f797fe24f2
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-15 00:43:28 +0300

    dasfaf

--- src/server.js
+++ src/server.js
@@ -39,7 +39,6 @@ const Widget = require('./models/widget')
 //const Usage = require('./models/usage')
 const ShopRedact = require('./models/shopRedact')
 const Billing = require('./models/billing')
-const { data } = require('jquery')
 
 var update = {}
 
@@ -60,6 +59,61 @@ server.use(router.routes());
 // })
 
 const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+
+router.get('/privacy', (ctx) => {
+  ctx.body = `
+  <html>
+  <head>
+  <title>
+      Privacy Policy
+  </title>
+  </head>
+  <body style="padding: 40px 25px;font-family: sans-serif;">
+  <h1 class="entry-title" itemprop="headline">Privacy Policy</h1>
+  <div class="entry-content clear" itemprop="text">
+      <div class="elementor-text-editor elementor-clearfix"><p><!-- wp:heading --><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>Garoo Apps provide applications "the Service" to merchants who use Shopify to power their stores. This Privacy Policy describes how personal information is collected, used, and shared when you install or use the App in connection with your Shopify-supported store.</p>
+          <div>
+              <h3 style="font-size: 2.14286rem; font-style: normal;">What personal data we collect</h3>
+          </div>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>When you install the App, we are automatically able to access certain types of information from your Shopify account:&nbsp;<a href="https://help.shopify.com/api/getting-started/authentication/oauth/scopes">The full list available through Shopify can be seen here</a>.</p>
+          <p><!-- /wp:paragraph --><!-- wp:paragraph --></p>
+          <p>To explicity view permissions each app has - please click the "View Details" link from the Shopify apps page within your Shopify admin</p>
+          <p>We collect data such as how and when you access your account (access logs), including information about the device and browser you use, your network connection and your IP address, the configuration you choose to make to our Apps, history of payments and logs of install/uninstall or any other actions or activity while using our Apps.</p>
+          <p>In certain cases, in order to solve technical issues, we might request access to some sections of your Shopify Admin. It is your responsibility to allow us access only to the sections we requested and once the technical problem is solved you agree to delete our access from your Shopify Admin.</p>
+          <p>As you visit or browse our websites, we collect information about the device and browser you use, your network connection, your IP address, and information about the cookies installed on your device. We also collect this information when you engage with us either by email or web form. We also collect any additional information that you might provide to us. We use this information, to provide and enhance our Services, including supporting or servicing your account, if applicable.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading {"level":3} --></p>
+          <h3>How Do We Use Your Personal Information?</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>We use the personal information we collect from you and your customers in order to provide the Service and to operate the App. Additionally, we use this personal information to: Communicate with you; Optimize or improve the App; and Provide you with information or advertising relating to our products or services.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading {"level":3} --></p>
+          <h3>Sharing Your Personal Information</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>Your information is never shared for the use of advertising. Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading {"level":3} --></p>
+          <h3>Your Rights</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through&nbsp;jorge.menzel@garooinc.com.</p>
+          <p>Additionally, if you are a European resident we note that we are processing your information in order to fulfill contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading {"level":3} --></p>
+          <h3>Data Retention</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>When you install the app, we will maintain your Information for our records unless and until you ask us to delete this information.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading --></p>
+          <h3>Changes</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading --></p>
+          <h3>Contact Us</h3>
+          <p><!-- /wp:heading --><!-- wp:paragraph --></p>
+          <p>For more information about our privacy practices or if you have questions, please contact us by e-mail at jorge.menzel@garooinc.com</p>
+          <p><!-- /wp:paragraph --><!-- wp:heading --></p>
+          <p><!-- /wp:heading --></p></div>
+  </div>
+</body></html>
+  `
+})
 
 router.post('/webhooks/customers/redact', webhook, (ctx) => {
   console.log('received webhook customers/redact: ', ctx.state.webhook);
