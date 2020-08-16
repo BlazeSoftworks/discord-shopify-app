22670da791c007755442c43800f3a69276c4bbdc
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-17 01:26:28 +0300

    plm

--- src/server.js
+++ src/server.js
@@ -63,6 +63,226 @@ server.use(router.routes());
 
 const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
 
+server.use(async (ctx) => {
+
+  const shopID = String(ctx.cookies.get("shopOrigin")).substr(0, String(ctx.cookies.get("shopOrigin")).length - 14);
+  console.log("shopID = ", shopID)
+  console.log(`update[${shopID}] = `, update[shopID])
+
+  //#region MONGODB ROUTES    
+
+  if ((update[shopID] == false || update[shopID] == undefined)) {// && shopID != undefined) {
+
+    //const cap = 1000
+
+    //console.log("MARELE VIERME")
+
+    // router.get(`/api/usageCreate/${shopID}`, async (ctx) => {
+    //   const item = (await Usage.find({ shopID }))[0]
+    //   const arr = await Usage.find({ shopID })
+    //   if (arr.length > 0 && item.counter == cap) {
+    //     const ur = await createUsageRecord(ctx, accessToken, shop, appSubscription.id)
+    //     console.log(ur.data.appUsageRecordCreate.userErrors)
+    //     ctx.status = 200
+    //     ctx.body = { status: 'success' }
+    //   }
+    //   else {
+    //     ctx.status = 200
+    //     ctx.body = { status: 'success' }
+    //   }
+    // })
+
+    // router.get(`/api/usageRecord/${shopID}`, async (ctx) => {
+    //   try {
+    //     const item = (await Usage.find({ shopID }))[0]
+    //     const arr = await Usage.find({ shopID })
+    //     //console.log(item)
+    //     if (arr.length > 0 && item.counter < cap) {
+    //       item.counter += 1;
+    //       await item.save()
+
+    //       ctx.status = 200
+    //       ctx.body = item;
+    //       console.log(item)
+    //     }
+    //     else if (arr.length > 0 && item.counter >= cap) {
+    //       item.counter = 1;
+    //       await item.save()
+
+    //       ctx.status = 200
+    //       ctx.body = item;
+    //       console.log(item)
+    //     }
+    //     else {
+    //       const id = new Usage({
+    //         counter: 1,
+    //         shopID
+    //       })
+    //       await id.save()
+    //       ctx.status = 200
+    //       ctx.body = id
+    //       console.log(id)
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //     ctx.status = 500
+    //     ctx.body = error
+    //   }
+    // })
+
+    console.log("A INTRAT AICI GEN WHAT COAIE")
+
+    router.get(`/api/discordID/${shopID}`, async (ctx) => {
+      console.log(ctx)
+      console.log()
+      console.log(ctx.URL)
+      console.log()
+      console.log(ctx.origin)
+      console.log()
+      console.log(ctx.originalUrl)
+
+      try {
+        const obj = await DiscordID.findOne({ shopID })
+        if (obj) {
+          ctx.status = 200
+          ctx.body = {
+            status: 'success',
+            data: {
+              serverID: obj.serverID,
+              channelID: obj.channelID,
+            }
+          }
+          //console.log(obj)
+        }
+        else {
+          ctx.status = 400
+          ctx.body = "Not found"
+        }
+      } catch (error) {
+        ctx.status = 500
+        ctx.body = error
+      }
+    });
+
+    router.post(`/api/discordID/${shopID}`, koaBody(), async (ctx) => {
+      if (ctx.cookies.get("shopOrigin")) {
+        try {
+          if ((await DiscordID.find({ shopID })).length > 0) {
+            const item = await DiscordID.findOneAndUpdate(shopID, {
+              serverID: ctx.request.body.serverID,
+              channelID: ctx.request.body.channelID,
+              shopID
+            }, { new: true, runValidators: true })
+            ctx.status = 200
+            ctx.body = "Item Updated";
+            //console.log(item)
+          }
+          else {
+            const id = new DiscordID({
+              serverID: ctx.request.body.serverID,
+              channelID: ctx.request.body.channelID,
+              shopID
+            })
+            await id.save()
+            ctx.status = 200
+            ctx.body = id
+          }
+        } catch (error) {
+          //console.log(error);
+          ctx.status = 500
+          ctx.body = error
+        }
+      } else {
+        ctx.status = 401
+        ctx.body = "Not authenticated"
+      }
+    })
+
+    router.get(`/api/widget/${shopID}`, async (ctx) => {
+      try {
+        const obj = await Widget.findOne({ shopID })
+        if (obj) {
+          ctx.body = {
+            status: 'success',
+            data: {
+              desktopPosition: obj.desktopPosition,
+              mobilePosition: obj.mobilePosition,
+              notificationText: obj.notificationText,
+              notificationTimeout: obj.notificationTimeout,
+              notificationAvatar: obj.notificationAvatar,
+              mobile: obj.mobile,
+              desktop: obj.desktop,
+              color: obj.color,
+              widgetEnabled: obj.widgetEnabled,
+              shopID
+            }
+          }
+          //console.log(obj)
+        }
+        else {
+          ctx.body = "Not found"
+        }
+      } catch (error) {
+        ctx.body = error
+      }
+    });
+
+    router.post(`/api/widget/${shopID}`, koaBody(), async (ctx) => {
+      if (ctx.cookies.get("shopOrigin")) {
+        //console.log(ctx.request.body)
+        try {
+          if ((await Widget.find({ shopID })).length > 0) {
+            const item = await Widget.findOneAndUpdate(shopID, {
+              desktopPosition: ctx.request.body.desktopPosition,
+              mobilePosition: ctx.request.body.mobilePosition,
+              notificationText: ctx.request.body.notificationText,
+              notificationTimeout: ctx.request.body.notificationTimeout,
+              notificationAvatar: ctx.request.body.notificationAvatar,
+              mobile: ctx.request.body.mobile,
+              desktop: ctx.request.body.desktop,
+              color: ctx.request.body.color,
+              widgetEnabled: ctx.request.body.widgetEnabled,
+              shopID
+            }, { new: true, runValidators: true })
+            ctx.status = 200
+            ctx.body = "Item Updated";
+            //console.log(item)
+          }
+          else {
+            const id = new Widget({
+              desktopPosition: ctx.request.body.desktopPosition,
+              mobilePosition: ctx.request.body.mobilePosition,
+              notificationText: ctx.request.body.notificationText,
+              notificationTimeout: ctx.request.body.notificationTimeout,
+              notificationAvatar: ctx.request.body.notificationAvatar,
+              mobile: ctx.request.body.mobile,
+              desktop: ctx.request.body.desktop,
+              color: ctx.request.body.color,
+              widgetEnabled: ctx.request.body.widgetEnabled,
+              shopID
+            })
+            await id.save()
+            ctx.status = 200
+            ctx.body = id
+          }
+        } catch (error) {
+          //console.log(error);
+          ctx.status = 500
+          ctx.body = error
+        }
+      } else {
+        ctx.status = 401
+        ctx.body = "Not authenticated"
+      }
+    })
+
+    update[shopID] = true
+  }
+
+  //#endregion
+
+})
+
 router.get('/test-script.js', async (ctx) => {
   //console.log("SE INCEARCA BA")
   try {
@@ -222,225 +442,10 @@ app.prepare().then(() => {
   );
 
   server.use(graphQLProxy({ version: ApiVersion.July20 }))
-
   server.use(verifyRequest());
+
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
-
-    const shopID = String(ctx.cookies.get("shopOrigin")).substr(0, String(ctx.cookies.get("shopOrigin")).length - 14);
-    console.log("shopID = ", shopID)
-    console.log(`update[${shopID}] = `, update[shopID])
-    //#region MONGODB ROUTES    
-
-    if ((update[shopID] == false || update[shopID] == undefined)) {// && shopID != undefined) {
-
-      //const cap = 1000
-
-      //console.log("MARELE VIERME")
-
-      // router.get(`/api/usageCreate/${shopID}`, async (ctx) => {
-      //   const item = (await Usage.find({ shopID }))[0]
-      //   const arr = await Usage.find({ shopID })
-      //   if (arr.length > 0 && item.counter == cap) {
-      //     const ur = await createUsageRecord(ctx, accessToken, shop, appSubscription.id)
-      //     console.log(ur.data.appUsageRecordCreate.userErrors)
-      //     ctx.status = 200
-      //     ctx.body = { status: 'success' }
-      //   }
-      //   else {
-      //     ctx.status = 200
-      //     ctx.body = { status: 'success' }
-      //   }
-      // })
-
-      // router.get(`/api/usageRecord/${shopID}`, async (ctx) => {
-      //   try {
-      //     const item = (await Usage.find({ shopID }))[0]
-      //     const arr = await Usage.find({ shopID })
-      //     //console.log(item)
-      //     if (arr.length > 0 && item.counter < cap) {
-      //       item.counter += 1;
-      //       await item.save()
-
-      //       ctx.status = 200
-      //       ctx.body = item;
-      //       console.log(item)
-      //     }
-      //     else if (arr.length > 0 && item.counter >= cap) {
-      //       item.counter = 1;
-      //       await item.save()
-
-      //       ctx.status = 200
-      //       ctx.body = item;
-      //       console.log(item)
-      //     }
-      //     else {
-      //       const id = new Usage({
-      //         counter: 1,
-      //         shopID
-      //       })
-      //       await id.save()
-      //       ctx.status = 200
-      //       ctx.body = id
-      //       console.log(id)
-      //     }
-      //   } catch (error) {
-      //     console.log(error);
-      //     ctx.status = 500
-      //     ctx.body = error
-      //   }
-      // })
-
-      console.log("A INTRAT AICI GEN WHAT COAIE")
-
-      router.get(`/api/discordID/${shopID}`, async (ctx) => {
-        console.log(ctx)
-        console.log()
-        console.log(ctx.URL)
-        console.log()
-        console.log(ctx.origin)
-        console.log()
-        console.log(ctx.originalUrl)
-
-        try {
-          const obj = await DiscordID.findOne({ shopID })
-          if (obj) {
-            ctx.status = 200
-            ctx.body = {
-              status: 'success',
-              data: {
-                serverID: obj.serverID,
-                channelID: obj.channelID,
-              }
-            }
-            //console.log(obj)
-          }
-          else {
-            ctx.status = 400
-            ctx.body = "Not found"
-          }
-        } catch (error) {
-          ctx.status = 500
-          ctx.body = error
-        }
-      });
-
-      router.post(`/api/discordID/${shopID}`, koaBody(), async (ctx) => {
-        if (ctx.cookies.get("shopOrigin")) {
-          try {
-            if ((await DiscordID.find({ shopID })).length > 0) {
-              const item = await DiscordID.findOneAndUpdate(shopID, {
-                serverID: ctx.request.body.serverID,
-                channelID: ctx.request.body.channelID,
-                shopID
-              }, { new: true, runValidators: true })
-              ctx.status = 200
-              ctx.body = "Item Updated";
-              //console.log(item)
-            }
-            else {
-              const id = new DiscordID({
-                serverID: ctx.request.body.serverID,
-                channelID: ctx.request.body.channelID,
-                shopID
-              })
-              await id.save()
-              ctx.status = 200
-              ctx.body = id
-            }
-          } catch (error) {
-            //console.log(error);
-            ctx.status = 500
-            ctx.body = error
-          }
-        } else {
-          ctx.status = 401
-          ctx.body = "Not authenticated"
-        }
-      })
-
-      router.get(`/api/widget/${shopID}`, async (ctx) => {
-        try {
-          const obj = await Widget.findOne({ shopID })
-          if (obj) {
-            ctx.body = {
-              status: 'success',
-              data: {
-                desktopPosition: obj.desktopPosition,
-                mobilePosition: obj.mobilePosition,
-                notificationText: obj.notificationText,
-                notificationTimeout: obj.notificationTimeout,
-                notificationAvatar: obj.notificationAvatar,
-                mobile: obj.mobile,
-                desktop: obj.desktop,
-                color: obj.color,
-                widgetEnabled: obj.widgetEnabled,
-                shopID
-              }
-            }
-            //console.log(obj)
-          }
-          else {
-            ctx.body = "Not found"
-          }
-        } catch (error) {
-          ctx.body = error
-        }
-      });
-
-      router.post(`/api/widget/${shopID}`, koaBody(), async (ctx) => {
-        if (ctx.cookies.get("shopOrigin")) {
-          //console.log(ctx.request.body)
-          try {
-            if ((await Widget.find({ shopID })).length > 0) {
-              const item = await Widget.findOneAndUpdate(shopID, {
-                desktopPosition: ctx.request.body.desktopPosition,
-                mobilePosition: ctx.request.body.mobilePosition,
-                notificationText: ctx.request.body.notificationText,
-                notificationTimeout: ctx.request.body.notificationTimeout,
-                notificationAvatar: ctx.request.body.notificationAvatar,
-                mobile: ctx.request.body.mobile,
-                desktop: ctx.request.body.desktop,
-                color: ctx.request.body.color,
-                widgetEnabled: ctx.request.body.widgetEnabled,
-                shopID
-              }, { new: true, runValidators: true })
-              ctx.status = 200
-              ctx.body = "Item Updated";
-              //console.log(item)
-            }
-            else {
-              const id = new Widget({
-                desktopPosition: ctx.request.body.desktopPosition,
-                mobilePosition: ctx.request.body.mobilePosition,
-                notificationText: ctx.request.body.notificationText,
-                notificationTimeout: ctx.request.body.notificationTimeout,
-                notificationAvatar: ctx.request.body.notificationAvatar,
-                mobile: ctx.request.body.mobile,
-                desktop: ctx.request.body.desktop,
-                color: ctx.request.body.color,
-                widgetEnabled: ctx.request.body.widgetEnabled,
-                shopID
-              })
-              await id.save()
-              ctx.status = 200
-              ctx.body = id
-            }
-          } catch (error) {
-            //console.log(error);
-            ctx.status = 500
-            ctx.body = error
-          }
-        } else {
-          ctx.status = 401
-          ctx.body = "Not authenticated"
-        }
-      })
-
-      update[shopID] = true
-    }
-
-    //#endregion
 
     ctx.respond = false;
     ctx.res.statusCode = 200;
