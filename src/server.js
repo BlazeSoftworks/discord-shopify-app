f29bbe0987b0c25969c00aab8ef5a61bb808d538
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-17 02:03:49 +0300

    plm

--- src/server.js
+++ src/server.js
@@ -86,7 +86,7 @@ server.use(async (ctx) => {
     shopID = String(ctx.cookies.get("shopOrigin")).substr(0, String(ctx.cookies.get("shopOrigin")).length - 14);
   }
   else {
-    shopID = String(ctx.request.header.origin).substr(0, String(ctx.request.header.origin).length - 14);
+    shopID = String(ctx.request.header.origin).substr(8, String(ctx.request.header.origin).length - 14);
   }
 
   console.log("shopID = ", shopID)
