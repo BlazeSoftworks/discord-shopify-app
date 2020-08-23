ef1fec100adb251094897e512829196dd3f0326e
BlazeSoftworks <blazesoftworks7@gmail.com>
2020-08-23 13:15:52 +0300

    fjaishfos

--- src/script.js
+++ src/script.js
@@ -22,7 +22,7 @@ if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elain
 // alert(JSON.stringify($("script[src*='discordify']")[0].src.split("shop=")[1]))
 
 var f = JSON.stringify($("script[src*='discordify']")[0].src.split("shop=")[1])
-const shopId = f.substring(1, f.length - 14);
+const shopId = f.substring(1, f.length - 15);
 
 var svID, chID
 
